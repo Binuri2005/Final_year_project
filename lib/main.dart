223903527401f032b87af8482234390023b0b8ca
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2025-02-21 23:43:56 +0000

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:app/screens/auth/login_page.dart';
 import 'package:app/screens/auth/signup_screen.dart';
 import 'package:app/screens/home_page.dart';
 import 'package:app/viewmodels/auth/auth_viewmodel..dart';
+import 'package:app/viewmodels/user/user.viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,7 +11,12 @@ import 'screens/intro_page.dart';
 
 void main() {
   runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => AuthViewModel()),
+    ChangeNotifierProvider(
+      create: (context) => AuthViewModel(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => UserViewModel(),
+    ),
   ], child: const MyApp()));
 }
 
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:app/social_skills_module/main_page.dart';
+import 'package:app/viewmodels/user/user.viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,123 +16,75 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView( // make sure the content is scrollable
+      body: SingleChildScrollView(
+        // make sure the content is scrollable
         child: Container(
           color: const Color(0xFFCADEEB),
           padding: const EdgeInsets.only(left: 30, right: 30, top: 5),
-          child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start,
-            children: [
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Welcome messsage
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Hello ${context.watch<UserViewModel>().user!.username}',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Image.asset(
+                      'assets/images/cartoon_cat.png',
+                      height: 60,
+                      width: 60,
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 2), // Space between the headings
 
-              // Welcome messsage 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Hello Binu!',
+                const Text(
+                  'Welcome to CareBloom! Choose a category to start learning and have fun improving your skills.',
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // Category heading
+                const Center(
+                  child: Text(
+                    'Category',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),
                   ),
-                  const SizedBox(width: 10), 
-                  Image.asset(
-                    'assets/images/cartoon_cat.png',
-                    height: 60, 
-                    width: 60, 
-                    fit: BoxFit.contain, 
-                  ),
-                ],
-              ),
-              const SizedBox(height: 2), // Space between the headings
-
-              const Text(
-                'Welcome to CareBloom! Choose a category to start learning and have fun improving your skills.',
-                style: TextStyle(
-                  fontSize: 13,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
                 ),
-              ),
-              const SizedBox(height: 20), 
+                const SizedBox(height: 8),
 
-              // Category heading 
-              const Center(
-                child: Text(
-                  'Category',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8), 
-
-              /////////// box one for SPEECH SKILLS MODULES ///////////////////
-              Center(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  padding: const EdgeInsets.all(20),
-                  height: 150,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFCC0C5), 
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                      color: Colors.black, 
-                      width: 1, 
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Speech Skills',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              ////////// SOCIAL SKILLS MODULE box //////////////////////
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            const SocialskillsPage(), // Navigate to SocialskillsPage
-                      ),
-                    );
-                  },
+                /////////// box one for SPEECH SKILLS MODULES ///////////////////
+                Center(
                   child: Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10),
+                    margin: const EdgeInsets.symmetric(vertical: 10),
                     padding: const EdgeInsets.all(20),
                     height: 150,
                     width: 300,
                     decoration: BoxDecoration(
-                      color: const Color(0xFFF8EE6C),
-                      borderRadius:
-                          BorderRadius.circular(15), 
+                      color: const Color(0xFFFCC0C5),
+                      borderRadius: BorderRadius.circular(15),
                       border: Border.all(
-                        color: Colors.black, 
-                        width: 1, 
+                        color: Colors.black,
+                        width: 1,
                       ),
                       boxShadow: [
                         BoxShadow(
@@ -143,7 +97,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                     child: const Center(
                       child: Text(
-                        'Social Skills',
+                        'Speech Skills',
                         style: TextStyle(
                           fontSize: 25,
                           fontWeight: FontWeight.bold,
@@ -153,45 +107,91 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-              ),
 
-              ////////////// box for daily life skills module /////////
-              Center(
-                child: Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10), 
-                  padding: const EdgeInsets.all(20),
-                  height: 150,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFF6C6FA),
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                      color: Colors.black, 
-                      width: 1, 
-                    ), 
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: const Offset(0, 3),
+                ////////// SOCIAL SKILLS MODULE box //////////////////////
+                Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              const SocialskillsPage(), // Navigate to SocialskillsPage
+                        ),
+                      );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      padding: const EdgeInsets.all(20),
+                      height: 150,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFF8EE6C),
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Daily Life Skills',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                      child: const Center(
+                        child: Text(
+                          'Social Skills',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+
+                ////////////// box for daily life skills module /////////
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    padding: const EdgeInsets.all(20),
+                    height: 150,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFF6C6FA),
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Daily Life Skills',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -227,4 +227,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
